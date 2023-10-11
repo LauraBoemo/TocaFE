56bcfeb007df2f2f6bc548b2d962807cf7cc90d7
lauraboemo <lauraboemo@gmail.com>
2023-10-10 23:35:54 -0300

    Adding Google Maps

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { useMemo } from "react"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+
 export default function Home() {
-  return (<h1>Toca</h1>)
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "" })
+
+  if (!isLoaded) return <div>Loading...</div>
+
+  return <Map />
+}
+
+function Map() {
+  return <GoogleMap zoom={10} center={{ lat: 44, lng: -80 }} mapContainerStyle={{ width: "100%", height: "100vh"}}></GoogleMap>
 }
