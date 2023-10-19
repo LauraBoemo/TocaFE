87378d54b737a63ea77a25731b016630512610ee
lauraboemo <lauraboemo@gmail.com>
2023-10-18 23:28:37 -0300

    Save the day

--- components/Logo/LogoTypography.tsx
+++ components/Logo/LogoTypography.tsx
@@ -5,7 +5,7 @@ const LogoTypography = () => {
     return (
         <Stack direction="column" alignItems="center" spacing={-0.5}>
             <Text fontSize='4xl' fontWeight={700} color="#95AA88" lineHeight="35px" ml="-6px" letterSpacing="-4px">
-                Toca
+                Toca 
             </Text>
             <Text fontSize='sm' mt={-1.5} fontWeight={200} color="#708265">
                 O seu local
